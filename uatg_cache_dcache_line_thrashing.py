19d5d653203e545f223b7c0b8fe796abdca73483
Hariprasad N <83636942+hariprasadn99@users.noreply.github.com>
2021-12-31 23:12:29 +0530

    Added comments wherever necessary

--- uatg_cache_dcache_line_thrashing.py
+++ uatg_cache_dcache_line_thrashing.py
@@ -7,9 +7,11 @@ import os
 from random import *
 
 class uatg_cache_dcache_fill(IPlugin):
+	""" Constructor to specify self variables required for test. """
 	def __init__(self):
 		super().__init__()
 		
+	""" Selects the parameters to make the test valid to be run on the DUT. """
 	def execute(self, core_yaml, isa_yaml):
 		_dcache_dict = core_yaml['dcache_configuration']
 		_dcache_en = _dcache_dict['instantiate']
@@ -18,13 +20,14 @@ class uatg_cache_dcache_fill(IPlugin):
 		self._block_size = _dcache_dict['block_size']
 		self._ways = _dcache_dict['ways']
 	
+	""" Returns a string of the asm file which is to be generated. """
 	def generate_asm(self) -> List[Dict[str, str]]:
 		asm = str()
-		asm += f'\tli t0, -10\n'
-		asm += f'\taddi t0, t0, t0\n'
-		asm += f'\tmul t0, t0, t0\n'
-		asm += f'\tdiv t0, t0, t0\n'
-		asm += f'\trem t0, t0, t0\n'
+		asm += f'\tli t0, -10\n' # Loaded -10 into t0
+		asm += f'\taddi t0, t0, t0\n' # Added the value of t0 to itself and stored it to the same register
+		asm += f'\tmul t0, t0, t0\n' # Multiplied the value of t0 with itself and stored it to the same register
+		asm += f'\tdiv t0, t0, t0\n' # Divided the value of t0 by itself and stored it to the same register
+		asm += f'\trem t0, t0, t0\n' # Found the remainder between the value of t0 and itself and stored it in the same register
 		
 		compile_macros = []
 		asm_data = str()
@@ -36,9 +39,11 @@ class uatg_cache_dcache_fill(IPlugin):
 			'compile_macros': compile_macros
 		}]	
 	
+	""" Returns false based on patterns required in the DUT logs. """
 	def check_log(self, log_file_path, reports_dir) -> bool:
 		return False
 
+	""" Returns a formatted string (converted to System Verilog) containing all coverpoints/assertions/covergroups necessary for test. """
 	def generate_covergroups(self, config_file) -> str:
 		sv = ""
 		return sv
